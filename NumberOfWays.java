2fdfdb4fa952603f46554e4724e5cb7065616bbd
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2021-09-05 00:09:16 +0530

    remove 1 from basecase

--- NumberOfWays.java
+++ NumberOfWays.java
@@ -3,7 +3,7 @@ public class NumberOfWays{
 	static int numberOfWays(int n){
 		if (n < 0)
 			return 0;
-		if (n == 0 || n == 1)
+		if (n == 0)
 			return 1;
 		return numberOfWays(n-1)+numberOfWays(n-2)+numberOfWays(n-3);
 	}
